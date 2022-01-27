9da6843330fb45eb02a029d461445553cb43f4f0
thierry <thierrypoupon@hotmail.com>
2022-01-27 11:12:39 +0100

    code cleanup

--- src/Services/withClass/photosService.js
+++ src/Services/withClass/photosService.js
@@ -1,29 +1,12 @@
 export default class photosService {
   static get8FirstPhotos(query) {
-    return (
-      fetch(
-        `https://api.unsplash.com/search/photos?query=${query}&client_id=TI96M4j8W_9hTLmsq2t883On8hdZx6cHKAoA_eBIJrE&per_page=9`,
-      )
-        .then(response => response.json())
-        // .then(data => data)
-        // // .catch(error => error)
-        // .catch(error => {
-
-        //     new Error(error)
-
-        // })
-        .catch(error => {
-          console.log('error message in photService ===', error.message)
-          return Promise.reject(
-            new Error(error.message),
-            // new Error("Aucun marvel lollll")
-          )
-        })
+    return fetch(
+      `https://api.unsplash.com/search/photos?query=${query}&client_id=TI96M4j8W_9hTLmsq2t883On8hdZx6cHKAoA_eBIJrE&per_page=9`,
     )
+      .then(response => response.json())
+      .catch(error => {
+        console.log('error message in photService ===', error.message)
+        return Promise.reject(new Error(error.message))
+      })
   }
 }
-
-// .catch(error => {
-//   return Promise.reject(
-//     new Error(`Aucun Marvel trouvé avec le nom "${name}"`),
-//   )
